b06429ca9e498e8dfba5288ccbc197994935c08b
Pablo <pablinelchulin@gmail.com>
2024-04-23 15:31:17 +0200

    project reset

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,17 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Frontend';
-  result = "";
-  isEnabled = true;
-
-  testFunction(): void {
-    if(this.isEnabled) {
-      this.result = "It is enabled!";
-    } else {
-      this.result = "";
-    }
-
-    this.isEnabled = !this.isEnabled;
-  }
+  title = 'Safeguard-Frontend';
 }
