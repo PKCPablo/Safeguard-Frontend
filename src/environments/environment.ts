9a3ee0f4db36ab59023b278270363cadc9b39c2f
Pablo <pablinelchulin@gmail.com>
2024-05-11 12:23:26 +0200

    update cognito client and pool ids

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     APIURL: 'https://h90rvfkk3a.execute-api.eu-central-1.amazonaws.com/dev',
-    CognitoUserPoolId: 'eu-central-1_a5zzava70',
-    CognitoClientId: '5m4up08quspst4ap8ufa5esn2q'
+    CognitoUserPoolId: 'eu-central-1_ADvCUGsDv',
+    CognitoClientId: '6pupq1kt4fucnd13ptpreopn5m'
 };
